2c09f15cc7b1fd16a4e4283c7996e7902df668bc
Username-Bogdan <127837806+Bogdan-Go-It@users.noreply.github.com>
2023-08-12 14:21:54 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const takeUlGallery = document.querySelector("ul.gallery");
+const galeryImeg = createGalleryImag(galleryItems);
+function createGalleryImag(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join("");
+}
+takeUlGallery.insertAdjacentHTML("afterbegin", galeryImeg);
+
+takeUlGallery.addEventListener("click", selectImg);
+
+function selectImg(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <div class="modal">
+          <img
+            class="gallery__image"
+            src="${event.target.dataset.source}"
+            alt="${event.target.alt}"
+          />
+      </div>
+  `);
+  return instance.show();
+}
+
+// instance.show();
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const takeUlGallery = document.querySelector("ul.gallery");
+const galeryImeg = createGalleryImag(galleryItems);
+function createGalleryImag(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}" 
+         alt="${description}" 
+         />
+      </a>
+    </li>`;
+    })
+    .join("");
+}
+
+takeUlGallery.insertAdjacentHTML("beforeend", galeryImeg);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: "true",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
